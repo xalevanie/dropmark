c5ad1e5847bb32bf54536b8ce4d6415e99843f3b
Madeline <mbrine@vcd-ceb131-01.eastern.ewu.edu>
2017-05-09 16:40:35 -0700

    API Template hooray

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,12 @@ var setStuff = {};
 var doStuff = function(data) {
 
   console.log(data.items);
-  console.log(data.items[0].link);
-  console.log(data.items[0].user_name);
-  console.log(data.items[0].thumbnails.cropped);
+ // console.log(data.items[0].link);
+ // console.log(data.items[0].user_name);
+ // console.log(data.items[0].thumbnails.cropped);
   
   // What do you want to do? Show (display on page) data? 
-  //showStuff(data);
+  showStuff(data);
   
 };
 
@@ -29,3 +29,25 @@ $.getJSON(getStuff, setStuff, doStuff);
 
 
 // Display it 
+
+
+var showStuff = function(data) {
+  
+    //put on page
+    $('h1').text(data.name);
+    $('p a').attr('href', data.url);
+
+
+
+//child Objects. May have many children
+ // Loop or cycle through all the data
+$.each(data.items, function( i ) {
+    // Disply data at each loop
+    
+    var stuff = '<p>' + data.items[i].name +'</p>';
+    $('#items').append(stuff);
+    
+    
+});
+    
+};

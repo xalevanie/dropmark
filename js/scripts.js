43fe988e91f3bb10139acdb4e8a88efd4ad67697
Madeline <mbrine@vcd-ceb131-01.eastern.ewu.edu>
2017-05-09 16:09:05 -0700

    dropbox api

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
-console.log('Whaaat');
+// configure JSON call
+//The API feed, the destination, the endpoint, where we want to go
+var getStuff = 'https://xalevanie.dropmark.com/420954.json';
+
+// Optional settings for private collections. This is public, no settings needed. 
+var setStuff = {};
+
+// Do stuff with returned data
+var doStuff = function(data) {
+
+  console.log(data.items);
+  console.log(data.items[0].link);
+  console.log(data.items[0].user_name);
+  console.log(data.items[0].thumbnails.cropped);
+  
+  // What do you want to do? Show (display on page) data? 
+  //showStuff(data);
+  
+};
+
+
+
+
+// Get our data, set
+$.getJSON(getStuff, setStuff, doStuff);
+
+
+// Display it 
